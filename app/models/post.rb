b328c49e6b16f9732b4f45ff55578d8a664023d9
Nikolas Escobal <niko.escobal@gmail.com>
2022-08-13 03:49:32 +0800

    Fix typos

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
     belongs_to :author, class_name: 'User'
     validates_presence_of :title, :content
-    has_many :article_categories, dependent: :destroy
-    has_many :categories, through: :article_categories
-    has_many :votes, dependent: :destroy
+    has_many :post_categories, dependent: :destroy
+    has_many :categories, through: :post_categories
+    has_many :likes, dependent: :destroy
+    has_many :comments, dependent: :destroy
 end
